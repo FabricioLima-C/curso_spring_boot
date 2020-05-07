e61259968cf887f99eefc512bc02481786c98b6d
Fabricio de Lima Cabral <fabriciocabral07@gmail.com>
2020-05-07 16:32:07 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,11 +2,20 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 //Essa interface possibilita que o objeto seja transformado em bits (0,1) e trafegue pela rede
+
+@Entity
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
