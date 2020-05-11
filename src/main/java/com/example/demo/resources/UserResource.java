cb0d455cd07007b9fb52ab04b10dfa6bd1799220
Fabricio de Lima Cabral <fabriciocabral07@gmail.com>
2020-05-11 17:34:10 -0300

    User insert

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.User;
 import com.example.demo.services.UserService;
@@ -34,6 +38,17 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	// Essa é a forma adequada para inserir um recurso no banco de dados pelo spring boot
+	// Pré-processamento na compilação do controlador, definindo que esse método recebe o método post do http
+	// Para o objeto chegar deserializado para o tipo User, usar a anotação @RequestBody
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// O http espera uma resposta com cabeçalho, ou seja, um endereço, essa resposta vem no formato URI, e é feito pela forma padrão do spring boot
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
